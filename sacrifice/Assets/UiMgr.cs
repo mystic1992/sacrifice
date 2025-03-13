522af3da61b51ccebd7ce5a76d2d9aca6e16c52e
Mario.C <mario.c@mechanist.co>
2025-03-13 20:51:36 +0800

    主界面滚动问题

--- sacrifice/Assets/UiMgr.cs
+++ sacrifice/Assets/UiMgr.cs
@@ -8,12 +8,20 @@ public class UiMgr: MonoBehaviour {
     public static UiMgr instance;
 
     public List<WndInfo> wndList = new List<WndInfo>();
+    private Dictionary<string, WndInfo> wndDic = new Dictionary<string, WndInfo>();
 
     private void Awake() {
         instance = this;
+        for(int i = 0; i < wndList.Count; i++) {
+            wndDic.Add(wndList[i].name, wndList[i]);
+        }
     }
 
     public void ShowWnd(string name) {
+        if (!wndDic.ContainsKey(name)) {
+            Debug.LogError("wnd not found:" + name);
+            return;
+        }
         for(int i = 0; i < wndList.Count; i++) {
             if (wndList[i].name == name) {
                 wndList[i].wnd.gameObject.SetActive(true);
